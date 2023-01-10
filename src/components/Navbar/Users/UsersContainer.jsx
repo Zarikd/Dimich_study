df0d769d21441225b41c6cd7655c91c74152ec31
Zarik <zar@zar.zar>
2023-01-11 00:12:12 +0300

    53 lessone i done

--- src/components/Navbar/Users/UsersContainer.jsx
+++ src/components/Navbar/Users/UsersContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { followAC, setUsersAC, unfollowAC } from '../../../redux/Users-reducer';
 
-import Users from './Users';
+import Users from './UsersC';
 
 
 
