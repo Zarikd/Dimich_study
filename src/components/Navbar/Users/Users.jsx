dcc28cdabacf2626c7270497c507285414648742
Zarikd <zarikd@mail.ru>
2023-01-10 17:47:35 +0300

    52 lessone is done

--- src/components/Navbar/Users/Users.jsx
+++ src/components/Navbar/Users/Users.jsx
@@ -1,41 +1,49 @@
+import axios from 'axios';
 import React from 'react';
 import s from './Users.module.css'
+import userPhoto from '../../../assets/images/User.jpg'
 
 const Users = (props) => {
     // debugger
-
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
-                followed: false, FullName: 'Dima', status: true, location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 2, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
-                followed: true, FullName: 'Sanya', status: false, location: { city: 'Shelkago', country: 'Russia' }
-            },
-            {
-                id: 3, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
-                followed: false, FullName: 'Atoxa', status: false, location: { city: 'Korolev', country: 'Russia' }
-            },
-            {
-                id: 4, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
-                followed: true, FullName: 'Dan', status: true, location: { city: 'Mitishi', country: 'Russia' }
-            },
-            {
-                id: 5, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
-                followed: false, FullName: 'Slam', status: true, location: { city: 'Himki', country: 'Russia' }
-            }
-        ]
-        )
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                // debugger;
+                props.setUsers(response.data.items);
+            })
+        };
+        // props.setUsers([
+        //     {
+        //         id: 1, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
+        //         followed: false, FullName: 'Dima', status: true, location: { city: 'Moscow', country: 'Russia' }
+        //     },
+        //     {
+        //         id: 2, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
+        //         followed: true, FullName: 'Sanya', status: false, location: { city: 'Shelkago', country: 'Russia' }
+        //     },
+        //     {
+        //         id: 3, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
+        //         followed: false, FullName: 'Atoxa', status: false, location: { city: 'Korolev', country: 'Russia' }
+        //     },
+        //     {
+        //         id: 4, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
+        //         followed: true, FullName: 'Dan', status: true, location: { city: 'Mitishi', country: 'Russia' }
+        //     },
+        //     {
+        //         id: 5, photoUrl: 'https://ruskino.ru/media/portret/009/sv3epEqsVGa6XN1cRjxoFlMFdWI.jpg',
+        //         followed: false, FullName: 'Slam', status: true, location: { city: 'Himki', country: 'Russia' }
+        //     }
+        // ]
+        // )
     }
     return (
         <div className={s.Users}>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img className={s.avatar} src={u.photoUrl} alt="" />
+                            <img className={s.avatar} src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
                         </div>
                         <div>
                             {u.followed ?
@@ -45,12 +53,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.FullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)
